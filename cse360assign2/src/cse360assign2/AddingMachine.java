58b5c39d6614fab8946a93e6585a8089950432f0
Ben <Ben@Office-PC>
2019-10-08 21:31:28 -0700

    Added functionaltiy to toString(), as well as to add(int value), and subtract(int value). add(int value) and subtract(int value) will now increase the total String.
    Made changes to the Javadoc notation above clear(), to better represent
    its function.
    
    Removed the test comment made earlier at the bottom of the program.

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -10,6 +10,9 @@ package cse360assign2;
 public class AddingMachine {
 	
 	private int total;
+	private String totalString = "";
+	private boolean isFirst = true;
+	
 	/**
 	 * This constructs an adding machine that 
 	 * will keep track of addition and subtraction
@@ -31,28 +34,41 @@ public class AddingMachine {
 	 * @param value the value to be added
 	 */
 	public void add (int value) {
+		if (isFirst == true) {
+			isFirst = false;
+			total += value;
+			totalString += value;
+		} else {
 		total += value;
+		totalString += " + " + value;
+		}
 	}
 	/**
 	 * This subtracts a value from the total.
 	 * @param value the value to be subtracted
 	 */
 	public void subtract (int value) {
+		if (isFirst == true) {
+			isFirst = false;
+			total -= value;
+			totalString += value;
+		} else {
 		total -= value;
+		totalString += " - " + value;
+		}
 	}
 	/**
 	 * This will return a string that represents the value of getTotal().
 	 * @return A string that represents the total
 	 */
 	public String toString () {
-		return "";
+		return totalString;
 	}
 	
 	/**
-	 * Resets the total to 0.
+	 * Resets total to 0 and totalString to "".
 	 */
 	public void clear() {
 		
 	}
-	// made a comment at the bottom of the program
 }
